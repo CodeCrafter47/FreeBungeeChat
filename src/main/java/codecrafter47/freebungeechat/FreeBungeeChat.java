09eb0e601f2d0d7eaef0c1fef6856721ff38676d
Florian Stober <Flo.e3@gmx.de>
2015-05-02 09:17:31 +0200

    use global logger for logging chat

--- src/main/java/codecrafter47/freebungeechat/FreeBungeeChat.java
+++ src/main/java/codecrafter47/freebungeechat/FreeBungeeChat.java
@@ -234,7 +234,7 @@ public class FreeBungeeChat extends Plugin implements Listener {
                 }
             }
             if(config.getBoolean("logChat", false)){
-                getLogger().info(player.getName() + ": " + message);
+                getProxy().getLogger().info(player.getName() + ": " + message);
             }
         } catch (Throwable th) {
             try {
@@ -265,7 +265,7 @@ public class FreeBungeeChat extends Plugin implements Listener {
                 }
             }
             if(config.getBoolean("logChat", false)){
-                getLogger().info(config.getString("consoleName", "SERVER") + ": " + message);
+                getProxy().getLogger().info(config.getString("consoleName", "SERVER") + ": " + message);
             }
         } catch (Throwable th) {
             getLogger().log(Level.SEVERE, "Error while processing chat message", th);
