42977055797b4291ed4d0dd919cdc3514c99dd23
Florian Stober <Flo.e3@gmx.de>
2015-07-06 12:43:21 +0200

    update ChatParser

--- src/main/java/codecrafter47/freebungeechat/ChatParser.java
+++ src/main/java/codecrafter47/freebungeechat/ChatParser.java
@@ -17,11 +17,11 @@ import java.util.regex.Pattern;
  * Created by florian on 25.01.15.
  */
 public class ChatParser {
-    private static Pattern pattern = Pattern.compile("(?ims)(?=\\n)|(?:[&\u00A7](?<color>[0-9A-FK-OR]))|" +
+    private static Pattern pattern = Pattern.compile("(?is)(?=\\n)|(?:[&\u00A7](?<color>[0-9A-FK-OR]))|" +
             "(?:\\[(?<tag>/?(?:b|i|u|s|nocolor|nobbcode)|(?:url|command|hover|suggest|color)=(?<value>(?:(?:[^]\\[]*)\\[(?:[^]\\[]*)\\])*(?:[^]\\[]*))|/(?:url|command|hover|suggest|color))\\])|" +
             "(?:\\[(?<implicitTag>url|command|suggest)\\](?=(?<implicitValue>.*?)\\[/\\k<implicitTag>\\]))");
 
-    private static Pattern strip_bbcode_pattern = Pattern.compile("(?ims)(?:\\[(?<tag>/?(?:b|i|u|s|nocolor|nobbcode)|(?:url|command|hover|suggest|color)=(?<value>(?:(?:[^]\\[]*)\\[(?:[^]\\[]*)\\])*(?:[^]\\[]*))|/(?:url|command|hover|suggest|color))\\])|" +
+    private static Pattern strip_bbcode_pattern = Pattern.compile("(?is)(?:\\[(?<tag>/?(?:b|i|u|s|nocolor|nobbcode)|(?:url|command|hover|suggest|color)=(?<value>(?:(?:[^]\\[]*)\\[(?:[^]\\[]*)\\])*(?:[^]\\[]*))|/(?:url|command|hover|suggest|color))\\])|" +
             "(?:\\[(?<implicitTag>url|command|suggest)\\](?=(?<implicitValue>.*?)\\[/\\k<implicitTag>\\]))");
 
     public static BaseComponent[] parse(String text) {
@@ -95,7 +95,7 @@ public class ChatParser {
             }
             if (group_tag != null && nobbcodeLevel <= 0) {
                 // [b]this is bold[/b]
-                if (group_tag.matches("(?i)^b$")) {
+                if (group_tag.matches("(?is)^b$")) {
                     forceBold++;
                     if (forceBold > 0) {
                         current.setBold(true);
@@ -103,7 +103,7 @@ public class ChatParser {
                         current.setBold(false);
                     }
                     parsed = true;
-                } else if (group_tag.matches("(?i)^/b$")) {
+                } else if (group_tag.matches("(?is)^/b$")) {
                     forceBold--;
                     if (forceBold <= 0) {
                         current.setBold(false);
@@ -113,7 +113,7 @@ public class ChatParser {
                     parsed = true;
                 }
                 // [i]this is italic[/i]
-                if (group_tag.matches("(?i)^i$")) {
+                if (group_tag.matches("(?is)^i$")) {
                     forceItalic++;
                     if (forceItalic > 0) {
                         current.setItalic(true);
@@ -121,7 +121,7 @@ public class ChatParser {
                         current.setItalic(false);
                     }
                     parsed = true;
-                } else if (group_tag.matches("(?i)^/i$")) {
+                } else if (group_tag.matches("(?is)^/i$")) {
                     forceItalic--;
                     if (forceItalic <= 0) {
                         current.setItalic(false);
@@ -131,7 +131,7 @@ public class ChatParser {
                     parsed = true;
                 }
                 // [u]this is underlined[/u]
-                if (group_tag.matches("(?i)^u$")) {
+                if (group_tag.matches("(?is)^u$")) {
                     forceUnderlined++;
                     if (forceUnderlined > 0) {
                         current.setUnderlined(true);
@@ -139,7 +139,7 @@ public class ChatParser {
                         current.setUnderlined(false);
                     }
                     parsed = true;
-                } else if (group_tag.matches("(?i)^/u$")) {
+                } else if (group_tag.matches("(?is)^/u$")) {
                     forceUnderlined--;
                     if (forceUnderlined <= 0) {
                         current.setUnderlined(false);
@@ -149,7 +149,7 @@ public class ChatParser {
                     parsed = true;
                 }
                 // [s]this is crossed out[/s]
-                if (group_tag.matches("(?i)^s$")) {
+                if (group_tag.matches("(?is)^s$")) {
                     forceStrikethrough++;
                     if (forceStrikethrough > 0) {
                         current.setStrikethrough(true);
@@ -157,7 +157,7 @@ public class ChatParser {
                         current.setStrikethrough(false);
                     }
                     parsed = true;
-                } else if (group_tag.matches("(?i)^/s$")) {
+                } else if (group_tag.matches("(?is)^/s$")) {
                     forceStrikethrough--;
                     if (forceStrikethrough <= 0) {
                         current.setStrikethrough(false);
@@ -167,7 +167,7 @@ public class ChatParser {
                     parsed = true;
                 }
                 // [color=red]huh this is red...[/color]
-                if (group_tag.matches("(?i)^color=.*$")) {
+                if (group_tag.matches("(?is)^color=.*$")) {
                     ChatColor color = null;
                     for (ChatColor color1 : ChatColor.values()) {
                         if (color1.getName().equalsIgnoreCase(group_value)) {
@@ -185,7 +185,7 @@ public class ChatParser {
                         current.setColor(ChatColor.WHITE);
                     }
                     parsed = true;
-                } else if (group_tag.matches("(?i)^/color$")) {
+                } else if (group_tag.matches("(?is)^/color$")) {
                     if (!colorDeque.isEmpty()) {
                         colorDeque.pop();
                         current.setColor(colorDeque.pop());
@@ -193,9 +193,9 @@ public class ChatParser {
                     parsed = true;
                 }
                 // [url=....]
-                if (group_tag.matches("(?i)^url=.*$")) {
+                if (group_tag.matches("(?is)^url=.*$")) {
                     String url = group_value;
-                    url = url.replaceAll("(?i)\\[/?nobbcode\\]", "");
+                    url = url.replaceAll("(?is)\\[/?nobbcode\\]", "");
                     if (!url.startsWith("http")) {
                         url = "http://" + url;
                     }
@@ -205,7 +205,7 @@ public class ChatParser {
                     parsed = true;
                 }
                 // [/url], [/command], [/suggest]
-                if (group_tag.matches("(?i)^/(?:url|command|suggest)$")) {
+                if (group_tag.matches("(?is)^/(?:url|command|suggest)$")) {
                     if (!clickEventDeque.isEmpty()) clickEventDeque.pop();
                     if (clickEventDeque.isEmpty()) {
                         current.setClickEvent(null);
@@ -215,23 +215,23 @@ public class ChatParser {
                     parsed = true;
                 }
                 // [command=....]
-                if (group_tag.matches("(?i)^command=.*")) {
-                    group_value = group_value.replaceAll("(?i)\\[/?nobbcode\\]", "");
+                if (group_tag.matches("(?is)^command=.*")) {
+                    group_value = group_value.replaceAll("(?is)\\[/?nobbcode\\]", "");
                     ClickEvent clickEvent = new ClickEvent(ClickEvent.Action.RUN_COMMAND, group_value);
                     clickEventDeque.push(clickEvent);
                     current.setClickEvent(clickEvent);
                     parsed = true;
                 }
                 // [suggest=....]
-                if (group_tag.matches("(?i)^suggest=.*")) {
-                    group_value = group_value.replaceAll("(?i)\\[/?nobbcode\\]", "");
+                if (group_tag.matches("(?is)^suggest=.*")) {
+                    group_value = group_value.replaceAll("(?is)\\[/?nobbcode\\]", "");
                     ClickEvent clickEvent = new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, group_value);
                     clickEventDeque.push(clickEvent);
                     current.setClickEvent(clickEvent);
                     parsed = true;
                 }
                 // [hover=....]...[/hover]
-                if (group_tag.matches("(?i)^hover=.*$")) {
+                if (group_tag.matches("(?is)^hover=.*$")) {
                     BaseComponent[] components1 = parse(group_value);
                     if (!hoverEventDeque.isEmpty()) {
                         // why is there no apache commons lib in bungee
@@ -247,7 +247,7 @@ public class ChatParser {
                     hoverEventDeque.push(hoverEvent);
                     current.setHoverEvent(hoverEvent);
                     parsed = true;
-                } else if (group_tag.matches("(?i)^/hover$")) {
+                } else if (group_tag.matches("(?is)^/hover$")) {
                     if (!hoverEventDeque.isEmpty()) hoverEventDeque.pop();
                     if (hoverEventDeque.isEmpty()) {
                         current.setHoverEvent(null);
@@ -259,7 +259,7 @@ public class ChatParser {
             }
             if (group_implicitTag != null && nobbcodeLevel <= 0) {
                 // [url]spigotmc.org[/url]
-                if (group_implicitTag.matches("(?i)^url$")) {
+                if (group_implicitTag.matches("(?is)^url$")) {
                     String url = group_implicitValue;
                     if (!url.startsWith("http")) {
                         url = "http://" + url;
@@ -270,14 +270,14 @@ public class ChatParser {
                     parsed = true;
                 }
                 // [command]/spawn[/command]
-                if (group_implicitTag.matches("(?i)^command$")) {
+                if (group_implicitTag.matches("(?is)^command$")) {
                     ClickEvent clickEvent = new ClickEvent(ClickEvent.Action.RUN_COMMAND, group_implicitValue);
                     clickEventDeque.push(clickEvent);
                     current.setClickEvent(clickEvent);
                     parsed = true;
                 }
                 // [suggest]/friend add [/suggest]
-                if (group_implicitTag.matches("(?i)^suggest$")) {
+                if (group_implicitTag.matches("(?is)^suggest$")) {
                     ClickEvent clickEvent = new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, group_implicitValue);
                     clickEventDeque.push(clickEvent);
                     current.setClickEvent(clickEvent);
@@ -285,19 +285,19 @@ public class ChatParser {
                 }
             }
             if (group_tag != null) {
-                if (group_tag.matches("(?i)^nocolor$")) {
+                if (group_tag.matches("(?is)^nocolor$")) {
                     nocolorLevel++;
                     parsed = true;
                 }
-                if (group_tag.matches("(?i)^/nocolor$")) {
+                if (group_tag.matches("(?is)^/nocolor$")) {
                     nocolorLevel--;
                     parsed = true;
                 }
-                if (group_tag.matches("(?i)^nobbcode$")) {
+                if (group_tag.matches("(?is)^nobbcode$")) {
                     nobbcodeLevel++;
                     parsed = true;
                 }
-                if (group_tag.matches("(?i)^/nobbcode$")) {
+                if (group_tag.matches("(?is)^/nobbcode$")) {
                     nobbcodeLevel--;
                     parsed = true;
                 }
